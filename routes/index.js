d1a729ad373f6f1ddc7b9ecb5f953c6201d32a64
Enrique Bonifácio Leite / IBSFGV <enrique@ibs.edu.br>
2019-11-11 10:09:35 -0300

    # Conectando a Marvel API e retornando resultados

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var axios = require('axios')
+const axios = require('axios')
 
 
 /* GET home page. */
@@ -8,30 +8,24 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/marvel', function(req, res, next) {
-
+router.get('/marvel', function(_, res) {
     /*For example, a user with a public key of "1234" and a private key of "abcd" 
     could construct a valid call as follows: 
     http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150 
     (the hash value is the md5 digest of 1abcd1234)*/
-
     const params = {
         ts: 1,
         apikey: "73409b595d40bb327ffaef09b12619f6",
         hash: "3bd06986c4a20c66e5091acd8ffefb9b"
     }
-    const url = `http://gateway.marvel.com/v1/public/characters?ts=${params['ts']}&apikey=${params['apikey']}&hash=${params['hash']}`;
-
-    //res.send(url)
-
-    /*  axios.get(url).then((teste) => {
-         console.log(teste)
-     }) */
-
-    res.send('teste')
-
-
-
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${params['ts']}&apikey=${params['apikey']}&hash=${params['hash']}`;
+    axios.get(url).then((response) => {
+        return response.data.data.results;
+    }).then(function(teste) {
+        res.send(teste)
+    }).catch(error => {
+        console.log("deu pau")
+    });
 })
 
 module.exports = router;
