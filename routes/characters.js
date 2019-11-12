5de1d0b38de3f2fe05a073342cfa0e2d38863d5d
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-11-11 22:52:28 -0300

    # iserindo classes getByName e getByID / Refatorando conexao

--- routes/characters.js
+++ routes/characters.js
@@ -5,11 +5,31 @@ const Characters = require('../store/Characters')
 router.get('/', function(_, res) {
     Characters.getAll().then((response) => {
         return response.data.data.results;
-    }).then(function(characters) {
+    }).then((characters) => {
         res.render('characters/index', { characters });
     }).catch(error => {
         console.log("deu pau")
-    });;
+    });
+})
+
+router.get('/name/:name', function(req, res) {
+    Characters.getByName(req.params.name).then((response) => {
+        return response.data.data.results;
+    }).then((characters) => {
+        res.render('characters/profile', { character: characters[0] });
+    }).catch(error => {
+        console.log("deu pau")
+    });
+})
+
+router.get('/id/:id', function(req, res) {
+    Characters.getByID(req.params.id).then((response) => {
+        return response.data.data.results;
+    }).then((characters) => {
+        res.render('characters/profile', { character: characters[0] });
+    }).catch(error => {
+        console.log("deu pau")
+    });
 })
 
 module.exports = router;
