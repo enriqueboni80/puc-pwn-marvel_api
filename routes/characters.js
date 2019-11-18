cf75e26f10745737435ad094c3d2003ddeae0a37
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-11-17 23:10:03 -0300

    # paginacao funcionando

--- routes/characters.js
+++ routes/characters.js
@@ -3,18 +3,29 @@ var router = express.Router();
 var Characters = require('../store/Characters');
 var pagination = require('pagination');
 
-function montarPaginacao(totalCharacters, limitPerPage) {
-    var paginator = pagination.create('search', { prelink: '/characters', current: 0, rowsPerPage: limitPerPage, totalResult: 100 });
+function montarPaginacao(totalCharacters, limitPerPage, currentPage) {
+    var paginator = pagination.create('search', {
+        prelink: '/characters',
+        current: currentPage,
+        rowsPerPage: limitPerPage,
+        totalResult: totalCharacters
+    });
     return paginator.render()
 }
 
 router.get('/', function(req, res) {
     let limitPerPage = 10
     let offset = (req.query.page - 1) * limitPerPage
+    if (offset < 0) {
+        offset = 0
+    }
     Characters.getAll(limitPerPage, offset).then((response) => {
         return response.data.data;
     }).then((characters) => {
-        res.render('characters/index', { characters, paginacao: montarPaginacao(Characters.total, limitPerPage) });
+        res.render('characters/index', {
+            characters,
+            paginacao: montarPaginacao(characters.total, limitPerPage, req.query.page)
+        });
     }).catch(error => {
         console.log("deu pau")
     });
