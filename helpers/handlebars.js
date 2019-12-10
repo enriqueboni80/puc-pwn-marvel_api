52d6d7d4873e6034f05cff1382fe324cab5c54e2
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-12-10 17:22:28 -0300

    # handleBars-helpers function divisivelPor - GridHTML para Characters

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -11,5 +11,32 @@ module.exports = {
         } else {
             return opts.inverse(this)
         }
+    },
+    montarGridHtmlCharacters: function(arrayOfThings, columns) {
+        var line = []
+        var result = []
+        var position
+        var x = 0
+        var html_result = ''
+
+        for (let index = 0; index < arrayOfThings.length; index++) {
+            if (index % columns === 0) {
+                line = []
+                position = x++
+            }
+            line.push(arrayOfThings[index])
+            result[position] = line
+        }
+
+        html_result += '<table>'
+        result.forEach((line, index) => {
+            html_result += '<tr>'
+            line.forEach(character => {
+                html_result += `<td>${character.name}</td>`
+            });
+            html_result += '</tr>'
+        });
+        html_result += '</table>'
+        return html_result
     }
 }
