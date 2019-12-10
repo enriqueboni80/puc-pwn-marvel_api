fd29ce92eec7027eebc66a29da9680f56dd8db77
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-12-10 15:40:58 -0300

    # handleBars-helpers function divisivelPor

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -5,21 +5,11 @@ module.exports = {
     bar: function() {
         return "BAR!";
     },
-    montarGrid: function(arrayOfThings, columns) {
-        var line = []
-        var result = []
-        var position
-        var x = 0
-
-        for (let index = 0; index < arrayOfThings.length; index++) {
-            if (index % columns === 0) {
-                line = []
-                position = x++
-            }
-            line.push(arrayOfThings[index].id)
-            result[position] = line
+    divisivelPor: function(columns, index) {
+        if (index % columns === 0) {
+            return "pin"
+        } else {
+            return index
         }
-
-        console.log(result)
     }
 }
